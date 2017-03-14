9a827d0945f1a16f72b9da7657483700789d5601
Adrifelcha <adrifelcha@gmail.com>
2017-03-14 10:48:08 -0600

    Creando carpeta con tareas de Ingles

---
+++ No_académico/Itzbsn/Animals_files/ajs.php
@@ -0,0 +1,17 @@
+(function(){
+var s = "<"+"script type=\"text/javascript\"><"+"!--\n";
+s += "google_ad_client = \"ca-pub-2469845013248145\";\n";
+s += "/* No mans land R side wide skyscraper */\n";
+s += "google_ad_slot = \"5380643784\";\n";
+s += "google_ad_width = 160;\n";
+s += "google_ad_height = 600;\n";
+s += "//-->\n";
+s += "<"+"/script>\n";
+s += "<"+"script type=\"text/javascript\"\n";
+s += "src=\"http://pagead2.googlesyndication.com/pagead/show_ads.js\">\n";
+s += "<"+"/script>\n";
+s += "<"+"div id=\'beacon_b2bc2e7e7c\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
+s += "<"+"img width=\"0\" height=\"0\" src=\"http://cat.ny.us.criteo.com/delivery/lg.php?cppv=1&cpp=h6FjLXxPM1ZhaFRiOElzZmZDNTE3c0NyLzZLWnEwN0htSWFka2VuUWZzV0JxTmRiaTMyRVB3L1oxNWZab0MyRlJ0d3BhTkJCTWVDTlcyOEdLT2V0cGRGUzMybkcxdktrcS9jQlZPTzBpUm0zNVZKMFd2UEtNeVRSbGhxOCtqaUNnV2tEeVFHZVBzNWlGRnBQWm4yYU8weWhGSjk0UWF3REE2Q2JvMmxTQ3ZJMERxMXJya1hBa0FYQ2gzZ1JiejlKOW9HZ0NWK0szTFNOOTc5ak1JREZJa3JiUml6VnNJU0sxMHNQckpPWVFvRk50UFJBPXw%3D\"/>\n";
+s += "<"+"/div>\n";
+s += "\n";
+document.write(s);})();
