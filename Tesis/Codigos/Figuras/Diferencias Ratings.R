d729e0d381d7cc091a9254158f312b2831f9df24
Adriana Chavez <adrifelcha@gmail.com>
2017-09-01 20:09:39 -0500

    Comparacion entre Puntajes de Confianza registrados

--- Tesis/Codigos/Figuras/Diferencias Ratings.R
+++ Tesis/Codigos/Figuras/Diferencias Ratings.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/Adrifelcha/Desktop/Felisa/Tesis/CSVs")
+setwd("C:/Users/Alejandro/Desktop/Felisa/Tesis/CSVs")
 rm(list=ls())
 #layout(matrix(1:2,ncol=1))
 dir()
@@ -27,23 +27,23 @@ Confidence <- c(C_AN, C_BN, C_BS, C_AS)
 
 
 
-#Ploteamos diferencias en D'
-matplot(Signal_mtx, type="b", lty=1, lwd=3, pch=21, col=c("green3"),
-        cex=1, ylim=c(2,6), xlim=c(0.75,2.25), xlab='Tipo de Estímulo', ylab='Tasa de Hits',
+#Ploteamos diferencias en Ratings
+matplot(Signal_mtx, type="b", lty=1, lwd=3, pch=21, col=c("darkslategray4"),
+        cex=1, ylim=c(4,6), xlim=c(0.75,2.25), xlab="Clase de Estímulo", ylab="Puntaje promedio",
         xaxp=c(1,2,1), las=1, labels=F)
-title('Experimento 1', outer = TRUE, font=1, line = -1.5)
-mtext('Diferencias en Hits',3,cex=1.2, col='green4')
-axis(2,at=c(0.5,0.6,0.7,0.8,0.9,1),labels=c("0.5","0.6","0.7","0.8","0.9","1"),las=1)
+title('Experimento 1', outer = TRUE, font=3, line = -1.5)
+mtext('Estímulos con Señal',3,cex=1.2, col='darkslategray4',font=2)
+axis(2,at=c(4,4.5,5,5.5,6),labels=c("4","4.5","5","5.5","6"),las=1)
 axis(1,at=c(1,2),labels=c("A","B"),las=1)
 
 
-matplot(Noise_mtx, type="b", lty=1, lwd=3, pch=21, col=c("indianred3"),
-        cex=1, ylim=c(1,4), xlim=c(0.75,2.25), xlab='Tipo de Estímulo', ylab='Tasa de F. Alarmas',
+matplot(Noise_mtx, type="b", lty=1, lwd=3, pch=21, col=c("deepskyblue4"),
+        cex=1, ylim=c(1,3.5), xlim=c(0.75,2.25), xlab='Clase de Estímulo', ylab='',
         xaxp=c(1,2,1), las=1, labels=F)
-mtext('Diferencias en F. Alarmas',3,cex=1.2, col='Red')
+mtext('Estímulos con Ruido',3,cex=1.2, font=2, col='deepskyblue4')
 
 axis(1,at=c(1,2),labels=c("A","B"),las=1)
-axis(2,at=c(0.0,0.1,0.2,0.3,0.4,0.5),labels=c("0","0.1","0.2","0.3","0.4","0.5"),las=1)
+axis(2,at=c(1,1.5,2,2.5,3,3.5),labels=c("1","1.5","2","2.5","3","3.5"),las=1)
 
 
 ###############################################
@@ -63,20 +63,19 @@ Noise_mtx <-matrix(data=c(C_AN,C_BN), nrow=2, ncol=20, byrow=TRUE)
 Confidence <- c(C_AN, C_BN, C_BS, C_AS)
 
 #Plot
-matplot(Signal_mtx, type="b", lty=1, lwd=3, pch=21, col=c("olivedrab3"),
-        cex=1, ylim=c(3,6), xlim=c(0.75,2.25), xlab='Tipo de Estímulos', ylab='Tasa de Hits',
+matplot(Signal_mtx, type="b", lty=1, lwd=3, pch=21, col=c("deeppink3"),
+        cex=1, ylim=c(2.5,6), xlim=c(0.75,2.25), xlab="Clase de Estímulo", ylab="Puntaje promedio",
         xaxp=c(1,2,1), las=1, labels=F)
-mtext('Diferencias en Hits',3,cex=1, col='olivedrab4')
+title('Experimento 2', outer = TRUE, font=3, line = -1.5)
+mtext('Estímulos con Señal',3,cex=1.2, col='deeppink3',font=2)
+axis(2,at=c(3,3.5,4,4.5,5,5.5,6),labels=c("3","3.5","4","4.5","5","5.5","6"),las=1)
+axis(1,at=c(1,2),labels=c("A","B"),las=1)
+
+
+matplot(Noise_mtx, type="b", lty=1, lwd=3, pch=21, col=c("deeppink4"),
+        cex=1, ylim=c(1,4), xlim=c(0.75,2.25), xlab='Clase de Estímulo', ylab='',
+        xaxp=c(1,2,1), las=1, labels=F)
+mtext('Estímulos con Ruido',3,cex=1.2, font=2, col='deeppink4')
 
 axis(1,at=c(1,2),labels=c("A","B"),las=1)
-axis(2,at=c(0.3,0.4,0.5,0.6,0.7,0.8,0.9,1),labels=c("0.3","0.4","0.5","0.6","0.7","0.8","0.9","1"),las=1)
-
-
-matplot(Noise_mtx, type="b", lty=1, lwd=3, pch=21, col=c("Red"),
-        cex=1, ylim=c(0,4), xlim=c(0.75,2.25), xlab='Tipo de Estímulos', ylab='Tasa de F. Alarmas',
-        xaxp=c(1,2,1), las=1, labels=F)
-title('Experimento 2', outer = TRUE, font=1, line = -1.5)
-mtext('Diferencias en F. Alarmas',3,cex=1, col='brown4')
-
-axis(1,at=c(1,2),labels=c("A","B"),las=1)
-axis(2,at=c(0.0,0.1,0.2,0.3,0.4,0.5),labels=c("0","0.1","0.2","0.3","0.4","0.5"),las=1)
+axis(2,at=c(1,1.5,2,2.5,3,3.5,4),labels=c("1","1.5","2","2.5","3","3.5","4"),las=1)
