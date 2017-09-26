c473a7f8c339548dacceb46e14bb31804caa3945
Adriana Chavez <adrifelcha@gmail.com>
2017-09-26 14:13:38 -0500

    Comenzando a trabajar en el experimento de reconocimiento facial

---
+++ Proyectos/Mario_BisecciónTemporal/SDT_NOparametrico.R
@@ -0,0 +1,6 @@
+#Codigo Base para Analisis NO paramétrico (SDT)
+#Autor: Adriana F. Chávez De la Peña
+#adrifelcha@gmail.com
+##########################################################
+
+rm(list=ls())  #Limpiamos el espacio de trabajo
