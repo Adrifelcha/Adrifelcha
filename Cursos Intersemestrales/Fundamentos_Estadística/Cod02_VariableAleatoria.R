ce4b531cea5a7dc074773c12eb5500436f82d64a
Adriana Chavez <adrifelcha@gmail.com>
2018-04-10 15:00:32 -0500

    Preparando codigos en R

--- Cursos Intersemestrales/Fundamentos_Estadística/Cod02_VariableAleatoria.R
+++ Cursos Intersemestrales/Fundamentos_Estadística/Cod02_VariableAleatoria.R
@@ -1,29 +1,50 @@
-x <- seq(0, 1, by=0.01)
-f <- 6*x*(1-x)
-plot(x, f, type="l", lwd=2, ylab="", col="red")
+#####################
+###### Código 1
+###### Representación gráfica de una función
+#####################
+
+x <- seq(0, 1, by=0.01)   #Definimos la recta real
+f <- 6*x*(1-x)            #Especificamos la funcion y=f(x)
+plot(x, f, type="l", lwd=2, ylab="", col="red")   #Ploteamos la relación x-y
 
 
+#####################
+###### Código 2
+###### Representación de dos funciones (simultáneamente)
+#####################
 
-x <- seq(0, 1, by=0.01)
-f <- 6*x*(1-x)
-g <- 18*x*(1-x)^4
+x <- seq(0, 1, by=0.01)     #Recta real
+f <- 6*x*(1-x)              #Función 1
+g <- 18*x*(1-x)^4           #Función 2
 plot(x, f, type="l", lwd=2, ylab="", ylim=c(0,1.6), col="red")
-lines(x, g, lwd=2, col="blue")
+lines(x, g, lwd=2, col="blue")   #Sobreponemos una línea que represente Función 2
 legend(0.75, 1.6, c("f(x)", "g(x)"), lty=c(1,1), lwd=2, col=c("red", "blue"))
 
 
+#####################
+###### Código 3
+###### Encontrar el punto máximo de una función (Moda)
+#####################
 g <- function(x) 18 * x * (1-x)^4
 fit <- optimize(g, c(0, 1), maximum=TRUE)
 print(fit)
 
+plot(g, type="l", lwd=2, ylab="", col="purple")   #Ploteamos la relación x-y
 
+
+
+#####################
+###### Código 1
+#####################
 
 f1 <- function(x) {6*x*(1-x)}
 integral <- integrate(f1, lower = 0, upper = 1)
 print(integral)
 
 
-
+#####################
+###### Código 1
+#####################
 xfx <- function(x) {6*x^2*(1-x)}
 Ex <- integrate(xfx, lower = 0, upper = 1)
 print(Ex)
--- Learning R/R - Basics/Manejo de datos en csv.R
+++ Learning R/R - Basics/Manejo de datos en csv.R
@@ -1,33 +1,75 @@
 ###################################################
-# Manejo de datos extraídos de un csv
+# Manejo de datos (CSV) con R
+# por Adriana F. Chávez
+# adrifelcha@gmail.com
 ###################################################
 
-setwd("C:/Users/Adriana/Desktop/Felisa/Learning R/R - Basics")
+#Empezamos nuestro código especificando el escritorio de trabajo (Working directory: wd)
+setwd("C:/Users/Alejandro/Desktop/Felisa/Learning R/R - Basics")
+#Borramos todas las variables y valores almacenados en consola
 rm(list=ls())
+#Comprobamos los archivos contenidos en nuestro wd
 dir()
 
+#Identificamos el archivo que contiene los datos a trabajar
 datos <-'Datos_Example2.csv'
+#Recogemos los datos del archivo
 granja <- read.csv(datos)
 
 
-promedio_accidentes <- mean(granja$Accidentes)
-print(promedio_accidentes)
 
-promedio_calificacion <- mean(granja$Calificación)
-print(promedio_calificacion)
+##################
+##################
+#  Leemos CSV    #
+##################
+##################
 
-promedio_calificacion <- round(promedio_calificacion,3)
-print(promedio_calificacion)
 
-Año <- granja$Tiempo..Año.
+#############¿Qué queremos saber?
+
+#Promedios
+mean(granja$Accidentes)
+#Valor Máximo
+max(granja$Accidentes)
+#Valor Minimo
+min(granja$Accidentes)
+#Desviacion Estandar
+sd(granja$Accidentes)
+
+
+############# Declaramos variables con nuestros datos
+accidentes <- granja$Accidentes
 Visitas <- granja$Visitas
+Calificación <- granja$Calificación
 Conejos <- granja$Conejos
+Cerdos <- granja$Cerdos
+Caballos <- granja$Caballos
+Año <- granja$Tiempo..Año.
 
-plot(Año,Visitas)
 
-plot(Año[1:9],Conejos[1:9], ylim=c(0,5000))
+#Creamos arreglos
+Visitas_Anuales <- cbind(Año,Visitas)
+print(Visitas_Anuales)
 
-visitasperaño <- c(Año,Visitas)
+visitas_anuales <- rbind(Año,Visitas)
+print(visitas_anuales)
 
-barplot(Visitas, xlab="Años", ylab="Visitas", col=rainbow(length(Año)))
-axis(1,at=c(0.5,1.7,2.9,4,5,6,7,8,9,10,11,12), labels= Año)
+promedios <- data.frame(round(cbind(mean(accidentes), mean(Visitas), mean(Calificación), mean(Conejos), mean(Cerdos), mean(Caballos)),3))
+colnames(promedios) <- c("Accidentes","Visitas","Calif", "Conejos", "Cerdos", "Caballos")
+print(promedios)
+
+
+
+
+
+##################
+##################
+#  Leemos CSV    #
+##################
+##################
+plot(Año,Visitas, main="Visitas anuales")
+
+plot(Visitas_Anuales, main="Visitas anuales")
+
+barplot(Visitas, xlab="Años", ylab="Visitas", col=rainbow(length(Año)), main="Visitas anuales")
+axis(1,at=c(0.6,1.9,3.1,4.3,5.5,6.7,7.9,9.1,10.3,11.5,12.7,13.9), labels= Año)
