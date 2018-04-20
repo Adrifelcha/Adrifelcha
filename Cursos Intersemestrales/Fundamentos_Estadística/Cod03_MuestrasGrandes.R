ad879eff438e62853f97b1804f6b7b926dbc3573
sandra de la peña <adrifelcha@gmail.com>
2018-04-20 01:07:56 -0500

    Muestras grandes

--- Cursos Intersemestrales/Fundamentos_Estadística/Cod03_MuestrasGrandes.R
+++ Cursos Intersemestrales/Fundamentos_Estadística/Cod03_MuestrasGrandes.R
@@ -4,18 +4,110 @@
 # adrifelcha@gmail.com
 ###################################################
 
-x <- seq(-6,6,.001)
 
-población <- dnorm(x,0,1)
 
-muestra_1 <- sample(población, 10)
+# Distribución  N O R M A L
+####################################
+####################################
+n_peque <- rnorm(5,0,1)
+n_media <- rnorm(20,0,1)
+n_grande <- rnorm(300,0,1)
 
-muestra_2 <- sample(población, 100)
+ns <- data.frame(round(cbind(mean(n_peque),mean(n_media), mean(n_grande)),3))
+colnames(ns) <- c("Pequeña","Media","Grande")
+print(ns)
 
-muestra_3 <- sample(población, 1000)
+peques<- c(NULL)
+medias<- c(NULL)
+grandes<- c(NULL)
 
-muestra_a <- rnorm(10,0,1)
+for(i in 1:10){
+peques[i]<- mean(rnorm(5,0,1))
+medias[i]<- mean(rnorm(20,0,1))
+grandes[i]<- mean(rnorm(300,0,1))
+}
 
-muestra_b <- rnorm(100,0,1)
+cbind(peques,medias,grandes)
 
-muestra_v <- rnorm(1000,0,1)
+cbind(mean(peques),mean(medias),mean(grandes))
+
+
+
+
+
+
+
+# Distribución  P O I S S O N
+####################################
+####################################
+n_peque <- rpois(5,10)
+n_media <- rpois(20,10)
+n_grande <- rpois(300,10)
+
+ns <- data.frame(round(cbind(mean(n_peque),mean(n_media), mean(n_grande)),3))
+colnames(ns) <- c("Pequeña","Media","Grande")
+print(ns)
+
+peques<- c(NULL)
+medias<- c(NULL)
+grandes<- c(NULL)
+
+for(i in 1:10){
+  peques[i]<- mean(rpois(5,10))
+  medias[i]<- mean(rpois(20,10))
+  grandes[i]<- mean(rpois(300,10))
+}
+
+cbind(peques,medias,grandes)
+
+cbind(mean(peques),mean(medias),mean(grandes))
+
+
+
+
+# Distribución  P O I S S O N
+####################################
+####################################
+n_peque <- rexp(5,10)
+n_media <- rexp(20,10)
+n_grande <- rexp(300,10)
+
+ns <- data.frame(round(cbind(mean(n_peque),mean(n_media), mean(n_grande)),3))
+colnames(ns) <- c("Pequeña","Media","Grande")
+print(ns)
+
+peques<- c(NULL)
+medias<- c(NULL)
+grandes<- c(NULL)
+
+for(i in 1:10){
+  peques[i]<- mean(rexp(5,10))
+  medias[i]<- mean(rexp(20,10))
+  grandes[i]<- mean(rexp(300,10))
+}
+
+cbind(peques,medias,grandes)
+
+cbind(mean(peques),mean(medias),mean(grandes))
+
+1/10
+
+
+
+#############################################################################
+#############################################################################
+#############################################################################
+#############################################################################
+#############################################################################
+# Teorema del Lìmite central
+
+v_normal <- rnorm(5000,0,1)
+hist(v_normal,breaks=50, main="Variable Normal")
+
+v_binom <- rbinom(5000,500,0.5)
+hist(v_binom, main="Variable Binomial", breaks=50)
+
+v_pois <- rpois(5000,10)
+hist(v_pois, main="Variable Poisson", breaks=50)
+
+
