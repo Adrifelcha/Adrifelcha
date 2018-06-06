85f35ff460ebbe2cafcba486c08ad62ade7698a0
Adriana Felisa Chavez de la Pena <afchavez@inee.edu.mx>
2018-06-05 19:04:07 -0500

    Kari

--- Proyectos/Kari/SDT_CodigoAnalisisYGraficacion.R
+++ Proyectos/Kari/SDT_CodigoAnalisisYGraficacion.R
@@ -27,53 +27,102 @@ for(i in 1:length(data$Participante)){
   }
 }
 
+
 Total_S <- 50
 Total_R <- 50
 
-HR_N <- Hits_Neutro/Total_S
-Fr_N <-FA_Neutro/Total_R
-HR_E <- Hits_Emo/Total_S
-Fr_E <- FA_Emo/Total_R
 
+Fr_N <- NULL
+Fr_E <- NULL
+HR_N <- NULL
+HR_E <- NULL
+
+for(i in 1:length(FA_Neutro)){
+  if(FA_Neutro[i]==0){
+    Fr_N[i]<-.01
+    #FA_rate[i]<-(FA[i]+1)/Noise[i]
+  } else {
+    Fr_N[i]<-FA_Neutro[i]/Total_R}
+  
+  if(Hits_Neutro[i]==Total_S){
+    HR_N[i]<-0.99
+    #Hit_rate[i]<-(Hits[i]-1)/Signal[i]
+  } else {
+    HR_N[i]<-Hits_Neutro[i]/Total_S}}
+
+for(i in 1:length(FA_Emo)){
+  if(FA_Emo[i]==0){
+    Fr_E[i]<-.01
+    #FA_rate[i]<-(FA[i]+1)/Noise[i]
+  } else {
+    Fr_E[i]<-FA_Emo[i]/Total_R}
+  
+  if(Hits_Emo[i]==Total_S){
+    HR_E[i]<-0.99
+    #Hit_rate[i]<-(Hits[i]-1)/Signal[i]
+  } else {
+    HR_E[i]<-Hits_Neutro[i]/Total_S}}
 
 
 
 #####Estimación Paramétrica
 
 ## Grupo: Lectura Neutra
-k<-qnorm(1-Fr_N,0,1)   #Calculamos la localizacion del Criterio
-d<-qnorm(h_rate,0,1)-qnorm(fa_rate,0,1)     #Calculamos d'
-c<-k-(d/2)                                  #Calculamos el Sesgo c
-beta<-dnorm(k,d,1)/dnorm(k,0,1)             #Calculamos el Sesgo Beta
+k_N<-qnorm(1-Fr_N,0,1)   #Calculamos la localizacion del Criterio
+d_N<-qnorm(HR_N,0,1)-qnorm(Fr_N,0,1)     #Calculamos d'
+c_N<-k_N-(d_N/2)                                  #Calculamos el Sesgo c
+beta_N<-dnorm(k_N,d_N,1)/dnorm(k_N,0,1)             #Calculamos el Sesgo Beta
 
 ## Grupo: Lectura Emotiva
-k<-qnorm(1-fa_rate,0,1)   #Calculamos la localizacion del Criterio
-d<-qnorm(h_rate,0,1)-qnorm(fa_rate,0,1)     #Calculamos d'
-c<-k-(d/2)                                  #Calculamos el Sesgo c
-beta<-dnorm(k,d,1)/dnorm(k,0,1)             #Calculamos el Sesgo Beta
+k_E<-qnorm(1-Fr_E,0,1)   #Calculamos la localizacion del Criterio
+d_E<-qnorm(HR_E,0,1)-qnorm(Fr_E,0,1)     #Calculamos d'
+c_E<-k_E-(d_E/2)                                  #Calculamos el Sesgo c
+beta_E<-dnorm(k_E,d_E,1)/dnorm(k_E,0,1)             #Calculamos el Sesgo Beta
 
 
+
+d_N<-round(d_N,5)   #Redondeamos el valor de d' a 5 decimales para facilitar su representacion
+k_N<-round(k_N,5)   #Redondeamos el valor del criterio a 5 decimales para facilitar su representacion
+c_N<-round(c_N,5)   #Redondeamos el valor del Sesgo c a 5 decimales para facilitar su representacion
+beta_N<-round(beta_N,5) #Redondeamos el valor del Sesgo Beta a 5 decimales para facilitar su representacion
+
+
+d_E<-round(d_E,5)    #Redondeamos el valor de d' a 5 decimales para facilitar su representacion
+k_E<-round(k_E,5)    #Redondeamos el valor del criterio a 5 decimales para facilitar su representacion
+c_E<-round(c_E,5)     #Redondeamos el valor del Sesgo c a 5 decimales para facilitar su representacion
+beta_E<-round(beta_E,5) #Redondeamos el valor del Sesgo Beta a 5 decimales para facilitar su representacion
+
+
+########################################
+#  P L O T E A M O S           #########
+########################################
 soporte <- seq(-6,6,.05)        #Especificamos el Soporte de nuestras distribuciones
-d_ruido <- dnorm(soporte,0,1)   #Definimos nuestra distribucion de Ruido de acuerdo a la teoria (Media=0 y DV=1)
-d_senal <- dnorm(soporte,d,1)   #Definimos la distribucion de Se??al, con media en d'
 
-plot(soporte,d_ruido,type='l')             #Dibujamos la distribucion de ruido
-lines(soporte,d_senal,type='l',col='blue') #Dibujamos la distribucion de Se??al
-abline(v=k,col='red')                      #Dibujamos el criterio
-abline(v=d/2,col='blue',lty=2)             #Se??alamos la localizacion Optima (sin sesgo) del criterio
+for(a in 1:length(d_N)){
+d_ruido_N <- dnorm(soporte,0,1)   #Definimos nuestra distribucion de Ruido de acuerdo a la teoria (Media=0 y DV=1)
+d_senal_N <- dnorm(soporte,d_N[a],1)   #Definimos la distribucion de Se??al, con media en d'
 
+d_ruido_E <- dnorm(soporte,0,1)   #Definimos nuestra distribucion de Ruido de acuerdo a la teoria (Media=0 y DV=1)
+d_senal_E <- dnorm(soporte,d_E[a],1)   #Definimos la distribucion de Se??al, con media en d'
 
-d<-round(d,5)   #Redondeamos el valor de d' a 5 decimales para facilitar su representacion
-k<-round(k,5)   #Redondeamos el valor del criterio a 5 decimales para facilitar su representacion
-c<-round(c,5)   #Redondeamos el valor del Sesgo c a 5 decimales para facilitar su representacion
-beta<-round(beta,5) #Redondeamos el valor del Sesgo Beta a 5 decimales para facilitar su representacion
+layout(matrix(c(1:2), ncol=1))
 
-text(-4,0.39,paste("K = ",k))  #Imprimimos la localizacion del criterio 
-text(-5,0.34,paste("C = ",c))  #Imprimimos el valor del Sesgo C
-text(-4,0.29,paste("D' = ",d)) #Imprimimos el valor de la distancia entre las medias (D')
-text(-5,0.24,paste("Hit Rate = ",h_rate))            #Especificamos la Tasa de Hits
-text(-4,0.19,paste("False alarm Rate = ",fa_rate))   #Especificamos la Tasa de Falsas Alarmas
+plot(soporte,d_ruido_N,type='l')             #Dibujamos la distribucion de ruido
+lines(soporte,d_senal_N,type='l',col='blue') #Dibujamos la distribucion de Se??al
+abline(v=k_N[a],col='red')                      #Dibujamos el criterio
+abline(v=d_N[a]/2,col='blue',lty=2)             #Se??alamos la localizacion Optima (sin sesgo) del criterio
 
+plot(soporte,d_ruido_E,type='l')             #Dibujamos la distribucion de ruido
+lines(soporte,d_senal_E,type='l',col='blue') #Dibujamos la distribucion de Se??al
+abline(v=k_E[a],col='red')                      #Dibujamos el criterio
+abline(v=d_E[a]/2,col='blue',lty=2)             #Se??alamos la localizacion Optima (sin sesgo) del criterio
+
+text(-4,0.39,paste("K = ",k_N[a]))  #Imprimimos la localizacion del criterio 
+text(-5,0.34,paste("C = ",c_N[a]))  #Imprimimos el valor del Sesgo C
+text(-4,0.29,paste("D' = ",d_N[a])) #Imprimimos el valor de la distancia entre las medias (D')
+text(-5,0.24,paste("Hit Rate = ",HR_N[a]))            #Especificamos la Tasa de Hits
+text(-4,0.19,paste("False alarm Rate = ",Fr_N[a]))   #Especificamos la Tasa de Falsas Alarmas
+}
 
 ############ Curvas ROC
 
